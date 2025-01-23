143e89d62a7ea9d6aae0ad8ef8705151c21ba8ff
gilbarel1 <gilbare@post.bgu.ac.il>
2025-01-23 16:39:16 +0200

    upload tests

--- compiler.ml
+++ compiler.ml
@@ -2234,7 +2234,9 @@ let sprint_exprs' chan exprs =
        
          let compile_and_run_scheme_string file_out_base user =
            let init = file_to_string "init.scm" in
-           let source_code = init ^ "\n" ^ user in
+           let test = "" in
+           (*let test = file_to_string "torture-test-for-compiler-00.scm" in*)
+           let source_code = init ^ "\n" ^ test ^  "\n" ^ user in
            let sexprs = (PC.star Reader.nt_sexpr source_code 0).found in
            let exprs = List.map Tag_Parser.tag_parse sexprs in
            let exprs' = List.map Semantic_Analysis.semantics exprs in
